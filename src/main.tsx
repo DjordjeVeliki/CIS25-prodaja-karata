b906cc783558cfa209317550df1d972cbd6b5e2d
Uros Jevtic <jevtic.uros4@gmail.com>
2025-08-28 21:17:52 +0200

    style: dodat Bootstrap i osnovni container

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { KorpaKontekstProvajder } from "./kontekst/KorpaKontekst";
 import { AuthProvajder } from "./kontekst/AuthKontekst";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // za navbar toggler i collapse
+
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
